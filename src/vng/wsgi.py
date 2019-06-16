978a20471697356321e3a1fdd7d4173b4d1f7661
Alex de Landgraaf <alex@maykinmedia.nl>
2019-06-16 14:28:15 +0200

    Enabling dotenv for production secrets

--- src/vng/wsgi.py
+++ src/vng/wsgi.py
@@ -8,20 +8,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from django.core.wsgi import get_wsgi_application
-
-
-def init_newrelic():
-    if os.environ.get('PROJECT_ROOT'):
-        try:
-            import newrelic.agent
-            newrelic.agent.initialize(os.path.join(os.environ.get('PROJECT_ROOT'), 'newrelic.ini'), 'production')
-        except Exception as e:
-            print("Could not initialize New Relic APM, ignoring:")
-            print(e)
-
-
-# Enable New Relic on production
-# init_newrelic()
 
 application = get_wsgi_application()
