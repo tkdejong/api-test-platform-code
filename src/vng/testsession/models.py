2df19a499f9760be1c33c98504c8b23da17ad2e1
Alvise <alvise@maykinmedia.nl>
2019-06-20 14:54:33 +0200

    Add uuid session

--- src/vng/testsession/models.py
+++ src/vng/testsession/models.py
@@ -157,6 +157,7 @@ class QueryParamsScenario(models.Model):
 
 class Session(models.Model):
 
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
     name = models.CharField('Naam', max_length=30, unique=True, null=True)
     session_type = models.ForeignKey(SessionType, verbose_name='Sessie type', on_delete=models.PROTECT)
     started = models.DateTimeField('Gestart op', default=timezone.now)
