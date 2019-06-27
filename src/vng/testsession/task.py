17df8b387d96c33e024ed2148e10cdf3decb5388
Alvise <alvise@maykinmedia.nl>
2019-06-27 10:08:05 +0200

    Increase percentage

--- src/vng/testsession/task.py
+++ src/vng/testsession/task.py
@@ -138,7 +138,7 @@ def ZGW_deploy(session):
         app=session.name,
         containers=containers
     ).execute()
-    ip = external_ip_pooling(k8s, session, max_percentage=80)
+    ip = external_ip_pooling(k8s, session, n_trial=30, max_percentage=40)
     if ip is None:
         update_session_status(session, 'Impossible to deploy successfully, IP address not allocated')
         session.status = choices.StatusChoices.error_deploy
