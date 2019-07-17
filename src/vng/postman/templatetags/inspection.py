e44592a945e66b640e1a1d48c0503948b3d4b3c1
Alvise <alvise@maykinmedia.nl>
2019-07-17 11:05:17 +0200

    exception handler filter

--- src/vng/postman/templatetags/inspection.py
+++ src/vng/postman/templatetags/inspection.py
@@ -6,6 +6,16 @@ from django.utils.translation import ugettext_lazy as _
 register = template.Library()
 
 
+def exception_as_string(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except:
+            return ''
+    return wrapper
+
+
+@exception_as_string
 @register.filter
 def info(value):
     with open(value.path) as infile:
@@ -13,6 +23,7 @@ def info(value):
     return obj['info']['schema']
 
 
+@exception_as_string
 @register.filter
 def info_name(value):
     with open(value.path) as infile:
@@ -20,6 +31,7 @@ def info_name(value):
     return obj['info']['name']
 
 
+@exception_as_string
 @register.filter
 def info_calls(value):
     with open(value.path) as infile:
@@ -42,16 +54,19 @@ def info_calls(value):
     return total_calls
 
 
+@exception_as_string
 @register.filter
 def build_url(value):
     return value['request']['url']['raw']
 
 
+@exception_as_string
 @register.filter
 def build_method(value):
     return value['request']['method']
 
 
+@exception_as_string
 @register.filter
 def build_script(value):
     test = list(filter(lambda x: x['listen'] == 'test', value['event']))
