f94f094843f06a551dc04d892089c1ccd897b0c7
Alex de Landgraaf <alex@maykinmedia.nl>
2019-10-17 16:53:55 +0200

    Increasing loglevel for django

--- src/vng/conf/test.py
+++ src/vng/conf/test.py
@@ -66,7 +66,7 @@ LOGGING['loggers'].update({
     },
     'django': {
         'handlers': ['django'],
-        'level': 'INFO',
+        'level': 'WARNING',
         'propagate': True,
     },
     'django.security.DisallowedHost': {
