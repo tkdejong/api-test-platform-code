402495cb007003889493efcbe00b7449e283e060
Steven Bal <steven@maykinmedia.nl>
2019-12-02 11:02:04 +0100

    Provider test request now marked as failed if an assertion failed for the request
    or if an error occurred

--- src/vng/servervalidation/templatetags/tag_library.py
+++ src/vng/servervalidation/templatetags/tag_library.py
@@ -1,5 +1,7 @@
 from django import template
 
+from vng.postman.utils import get_call_result as _get_call_result
+
 register = template.Library()
 
 
@@ -11,3 +13,7 @@ def to_int(value):
 @register.filter
 def index(l, i):
     return l[int(i)]
+
+@register.filter
+def get_call_result(value):
+    return _get_call_result(value)
