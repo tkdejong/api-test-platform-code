2898bfd7c33ddd4e3f9eade973bab5a77cd635f5
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2020-10-30 11:17:24 +0100

    Paginate the needed things

--- src/vng/design_rules/views.py
+++ src/vng/design_rules/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import InvalidPage, Paginator
+from django.http.response import Http404
 from django.urls.base import reverse
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import ListView
 from django.views.generic.base import RedirectView
 from django.views.generic.detail import DetailView, SingleObjectMixin
@@ -14,6 +17,7 @@ from .models import DesignRuleTestSuite, DesignRuleSession
 class DesignRulesListView(LoginRequiredMixin, ListView):
     model = DesignRuleTestSuite
     template_name = "design_rules/list.html"
+    paginate_by = 20
 
 
 class DesignRulesCreateView(CreateView):
@@ -35,6 +39,47 @@ class DesignRulesDetailView(DetailView):
     model = DesignRuleTestSuite
     slug_url_kwarg = 'uuid'
     slug_field = 'uuid'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        test_suite = self.get_object()
+        paginator, page, queryset, is_paginated = self.paginate_queryset(test_suite.sessions.all(), 10)
+        context['paginator'] = paginator
+        context['page_obj'] = page
+        context['is_paginated'] = is_paginated
+        context['object_list'] = queryset
+        return context
+
+    def paginate_queryset(self, queryset, page_size):
+        """Paginate the queryset, if needed."""
+        paginator = self.get_paginator(
+            queryset, page_size, orphans=0,
+            allow_empty_first_page=True)
+        page_kwarg = 'page'
+        page = self.kwargs.get(page_kwarg) or self.request.GET.get(page_kwarg) or 1
+        try:
+            page_number = int(page)
+        except ValueError:
+            if page == 'last':
+                page_number = paginator.num_pages
+            else:
+                raise Http404(_("Page is not 'last', nor can it be converted to an int."))
+        try:
+            page = paginator.page(page_number)
+            return (paginator, page, page.object_list, page.has_other_pages())
+        except InvalidPage as e:
+            raise Http404(_('Invalid page (%(page_number)s): %(message)s') % {
+                'page_number': page_number,
+                'message': str(e)
+            })
+
+    def get_paginator(self, queryset, per_page, orphans=0,
+                      allow_empty_first_page=True, **kwargs):
+        """Return an instance of the paginator for this view."""
+        return Paginator(
+            queryset, per_page, orphans=orphans,
+            allow_empty_first_page=allow_empty_first_page, **kwargs)
 
 
 class DesignRuleSessionCreateView(SingleObjectMixin, RedirectView):
